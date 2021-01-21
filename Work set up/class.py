e5b110a4448483da8ee28f02a5e0149199881485
OusmaneKana <kanaman6@gmail.com>
2021-01-20 18:32:24 -0600

    Update class.py
    Semester course load udpdate

--- Work set up/class.py
+++ Work set up/class.py
@@ -9,19 +9,37 @@ def openSite(domain):
 		webbrowser.open("https:www.{}.com".format(site))
 
 def openFile(file): 
-	os.system("cd C:\\Users\\Ousmane Kana\\OneDrive - Houston Community College\\Fall 2020\\{} & start .".format(file))
+	os.system("cd C:\\Users\\Ousmane Kana\\OneDrive - North American University\\Spring 2021\\{} & start .".format(file))
 def openApp(app):
 	os.system("start {}".format(app))
 
 def chooseClass():
+	webbrowser.open("https://portal.na.edu/ics")
+	webbrowser.open("https://www.office.com/")
 	zoom =input("Do you have an Online session Today ?__")
 
 	if zoom.lower() in ("yes", "yup"):
-		os.system("cd C:\\Users\\Ousmane Kana\\AppData\\Roaming\\Zoom\\bin & start zoom.exe")
+		print ("Which class ?")
+		selection = input(" 1- Data Mining\n 2- Discrete Math \n 3-Programming Language \n 4 -Statistics \n 5-System Programming  \n -----> ")
+		selection = selection.split(',')
+		for course in selection:
+			if course =="1":
+				webbrowser.open("https://global.gotomeeting.com/join/213951021")
+			if course =="2":
+				webbrowser.open("https://www.gotomeet.me/hnguye01/math2317_spring2021")
+			if course =="3":
+				webbrowser.open("https://global.gotomeeting.com/join/894151789")
+			if course =="4":
+				webbrowser.open("https://www.gotomeet.me/hnguye01/math1312_spring2021")
+			if course =="5":
+				webbrowser.open("https://global.gotomeeting.com/join/408824989")
+			
+
+
 
 	print("\n\nWhat Class you have ?")
 
-	selection = input(" 1- Physic Lecture \n 2- Physics Lab \n 3-Computer Organization \n -----> ")
+	selection = input(" 1- Data Mining\n 2- Discrete Math \n 3-Programming Language \n 4 -Statistics \n 3-System Programming  \n -----> ")
 	if selection == "1":
 		webbrowser.open("https://eagleonline.hccs.edu/courses/140584")
 		openFile("Phys II")
@@ -29,6 +47,12 @@ def chooseClass():
 		webbrowser.open("https://eagleonline.hccs.edu/courses/133980")
 		openFile("Phys II lab")
 	elif selection =="3":
+		webbrowser.open("https://eagleonline.hccs.edu/courses/141914")
+		openFile("Computer Organization")
+	elif selection =="4":
+		webbrowser.open("https://eagleonline.hccs.edu/courses/141914")
+		openFile("Computer Organization")
+	elif selection =="5":
 		webbrowser.open("https://eagleonline.hccs.edu/courses/141914")
 		openFile("Computer Organization")
 
@@ -63,7 +87,7 @@ def main():
 	
 	
 	while True:
-		task= input("\n\nWhat's up? \n\t - Class \n\t - Coding \n\t - Finances \n\t - Tools \n\t - Tutoring \n------> ")
+		task= input("\n\nWhat's up? \n\t - Class \n\t - Coding \n\t - Finances \n\t - Tools \n ----> ")
 		if task.lower()== "class":
 			chooseClass()
 
@@ -86,11 +110,6 @@ def main():
 
 					openApp(name)
 					continue
-		elif task.lower() in ('tutor', 'tutoring'):
-			os.system("cd C:\\Users\\Ousmane Kana\\AppData\\Roaming\\Zoom\\bin & start zoom.exe")
-			openSite(("classroom.google","desmos","gmail"))
-			os.system("cd C:\\Users\\Ousmane Kana\\OneDrive\\Finances & start tutoringPayBook.xlsx")
-			break
 
 
 
